f5f03093564d55a774577555c24e2276d9b69d60
Viktor Borosnyai <vborosnyai@gmail.com>
2026-05-12 20:37:17 +0200

    Adding the missing variables

--- src/animals/fish.hpp
+++ src/animals/fish.hpp
@@ -8,7 +8,8 @@ class Fish : public Animal{
 public:
     Fish(uint64_t id, const std::string& name, const std::string& species, 
            uint64_t age, double weight, const std::string& enclosure, 
-           HealthStatus health_status) 
+           HealthStatus health_status, const std::string& feeding_type,
+       const std::string& habitat, double min_enclosure_size) 
         : Animal(id, name, species, age, weight, enclosure, health_status, feeding_type_, habitat_, min_enclosure_size_){}
     
     AnimalCategory get_category() const override;
--- src/animals/mammal.hpp
+++ src/animals/mammal.hpp
@@ -7,9 +7,11 @@ class Mammal : public Animal{
     
 public:
     Mammal(uint64_t id, const std::string& name, const std::string& species, 
-           uint64_t age, double weight, const std::string& enclosure, 
-           HealthStatus health_status) 
-        : Animal(id, name, species, age, weight, enclosure, health_status, feeding_type_, habitat_, min_enclosure_size_){}
+       uint64_t age, double weight, const std::string& enclosure, 
+       HealthStatus health_status, const std::string& feeding_type,
+       const std::string& habitat, double min_enclosure_size) 
+    : Animal(id, name, species, age, weight, enclosure, health_status, 
+             feeding_type, habitat, min_enclosure_size){}
     
     AnimalCategory get_category() const override;
 };
