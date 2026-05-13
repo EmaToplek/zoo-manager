1f3e3bf97deba03adda2652d6a6420e98e946b07
EmaToplek <toplek.ema0213@outlook.com>
2026-05-13 11:24:49 +0200

    fix: add tickhealth and helper methods to Animal

--- src/animals/animal.cpp
+++ src/animals/animal.cpp
@@ -1,24 +1,13 @@
 #include "animal.hpp"
 
-
-Animal::Animal(uint64_t id, const std::string& name, const std::string& species, 
-               uint64_t age, double weight, const std::string& enclosure, 
-               HealthStatus health_status, const std::string& feeding_type, 
-               const std::string& habitat, double min_enclosure_size) {
-    id_ = id;
-    name_ = name;
-    species_ = species;
-    age_ = age;
-    weight_ = weight;
-    enclosure_ = enclosure;
-    health_status_ = health_status;
-    feeding_type_ = feeding_type;
-    habitat_ = habitat;
-    min_enclosure_size_ = min_enclosure_size;
-    ticks_ = 0;   
-}
-
-
+Animal::Animal(uint64_t id, const std::string& name, const std::string& species, uint64_t age, double weight, 
+               const std::string& enclosure, HealthStatus health_status, const std::string& feeding_type, 
+               const std::string& habitat, double min_enclosure_size)
+    : id_(id), name_(name), species_(species), age_(age), weight_(weight),
+    enclosure_(enclosure), health_status_(health_status),
+    feeding_type_(feeding_type), habitat_(habitat),
+    min_enclosure_size_(min_enclosure_size)
+{}
 
 
 void Animal::tickhealth(){
