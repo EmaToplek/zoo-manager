6ccfa6e607dd91fcfc87d0767bd4febab611c075
EmaToplek <toplek.ema0213@outlook.com>
2026-05-14 00:05:36 +0200

    #45: add pure virtuals to all subclasses

--- src/animals/mammal.hpp
+++ src/animals/mammal.hpp
@@ -8,12 +8,14 @@ class Mammal : public Animal{
 public:
     Mammal(uint64_t id, const std::string& name, const std::string& species, 
        uint64_t age, double weight, const std::string& enclosure, 
-       HealthStatus health_status, const std::string& feeding_type,
-       const std::string& habitat, double min_enclosure_size) 
-    : Animal(id, name, species, age, weight, enclosure, health_status, 
-             feeding_type, habitat, min_enclosure_size){}
+       HealthStatus health_status);
     
     AnimalCategory get_category() const override;
+    std::string get_feeding_type() const override;
+    std::string get_habitat() const override;
+    double get_min_enclosure_size() const override;
+    std::string get_special_info() const override;
+
 };
 
 #endif
