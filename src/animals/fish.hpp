b1b1dba7ef3b4b2d8ebd2c0eaa1b712f6459d1c4
Viktor Borosnyai <vborosnyai@gmail.com>
2026-05-13 10:06:45 +0200

    Removing underscore.

--- src/animals/fish.hpp
+++ src/animals/fish.hpp
@@ -10,7 +10,7 @@ public:
            uint64_t age, double weight, const std::string& enclosure, 
            HealthStatus health_status, const std::string& feeding_type,
        const std::string& habitat, double min_enclosure_size) 
-        : Animal(id, name, species, age, weight, enclosure, health_status, feeding_type_, habitat_, min_enclosure_size_){}
+        : Animal(id, name, species, age, weight, enclosure, health_status, feeding_type, habitat, min_enclosure_size){}
     
     AnimalCategory get_category() const override;
 };
