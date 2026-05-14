287eb92cb5f1d389f771fee3b54a95a4a208bf4d
Viktor Borosnyai <vborosnyai@gmail.com>
2026-05-14 14:00:20 +0200

    #46 Add vector include, button_panel and left_panel_sizer_ variable,

--- src/gui/main_frame.hpp
+++ src/gui/main_frame.hpp
@@ -3,6 +3,7 @@
 #include <wx/wx.h>
 #include <wx/listctrl.h>
 #include "../app/animal_manager.hpp"
+#include <vector>
 
 class MainFrame : public wxFrame
 {
@@ -11,6 +12,8 @@ private:
     wxPanel* left_panel_;
     wxPanel* right_panel_;
     wxListCtrl* table_;
+    wxPanel* button_panel_; 
+    wxBoxSizer* left_panel_sizer_;
     Animal_Manager* animal_manager_; // manages animal list, load/save, filtering
     void fill_table(const std::vector<Animal*> animals);   
 
