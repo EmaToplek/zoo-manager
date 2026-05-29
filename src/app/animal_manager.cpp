02f155bea63fbd907f32400133e7ac259326cfd3
Viktor Borosnyai <vborosnyai@gmail.com>
2026-05-29 18:04:48 +0200

    #58 Implemented demo animals solution.

--- src/app/animal_manager.cpp
+++ src/app/animal_manager.cpp
@@ -7,22 +7,29 @@ void Animal_Manager::load()
 {
     //opens data.json and parses its contents
     std::ifstream file("../data.json");
-    nlohmann::json j;
-    file >> j;
-    
-    //for each entry in json array, extracts fields and calls add_animal()
-    for(auto& animal : j) {
-        uint64_t id = animal["id"];
-        std::string name = animal["name"];
-        std::string species = animal["species"];
-        std::string category = animal["category"];
-        uint64_t age = animal["age"];
-        double weight = animal["weight"];
-        std::string enclosure = animal["enclosure"];
-        std::string health = animal["health"];
-    
-        add_animal(id, name, species, category, age, weight, enclosure, health);
+    if(!file) {
+        std::cerr << "File doesn't exists!Adding demo animals!" << std::endl;
+        add_animal(1, "Leo", "Lion", "Mammal", 4, 192.0, "Savannah", "In Treatment");
+        add_animal(19, "Toad", "Amphibian", "Amphibian", 4, 0.4, "Pond", "Sick");
+    } else{
+        nlohmann::json j;
+        file >> j;
+        
+        //for each entry in json array, extracts fields and calls add_animal()
+        for(auto& animal : j) {
+            uint64_t id = animal["id"];
+            std::string name = animal["name"];
+            std::string species = animal["species"];
+            std::string category = animal["category"];
+            uint64_t age = animal["age"];
+            double weight = animal["weight"];
+            std::string enclosure = animal["enclosure"];
+            std::string health = animal["health"];
+        
+            add_animal(id, name, species, category, age, weight, enclosure, health);
+        }
     }
+    
 }
 
 
